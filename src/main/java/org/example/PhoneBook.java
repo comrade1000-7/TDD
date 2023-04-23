25d173ba19a600a943614316893fdf1b9f89d638
Vasily Parshentsev <vparshentsev@gmail.com>
2023-04-23 13:25:42 +0300

    Stub and tests for add

--- src/main/java/org/example/PhoneBook.java
+++ src/main/java/org/example/PhoneBook.java
@@ -1,4 +1,12 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class PhoneBook {
+    private Map<String, String> phoneBook = new HashMap<>();
+
+    public int add(String name, String number) {
+        return 0;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,3 +1,22 @@
-public class PhoneBookTest {
+import org.example.PhoneBook;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+
+public class PhoneBookTest {
+    private PhoneBook phoneBook = new PhoneBook();
+    @Before
+    public void setUp() {
+    }
+    @Test
+    public void shouldAddContact() {
+        int actual = phoneBook.add("Ivanov", "+79122343445");
+        int expected = 1;
+        assertEquals(expected, actual);
+    }
 }
